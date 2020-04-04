54460e4c5f0f005252dd0b9621daa99098365fcc
Jorge Ortega Valdez <jorge.ortega@wizeline.com>
2020-04-04 15:35:39 -0700

    change path

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,3 +1,3 @@
-echo env1=${BUCKET_NAME} >> ../../../.env
-echo env1=${SECRET_KEY} >> ../../../.env
-cat ../../../.env
+echo env1=${BUCKET_NAME} >> ../packages/android/test/.env
+echo env1=${SECRET_KEY} >> ../packages/android/test/.env
+cat ../packages/android/test/.env
